11465e36657c65f606060bab87faff7826b684ad
Michael Wood <michaelwood33311@icloud.com>
2023-05-09 13:21:13 -0400

    working up through full transcript cleaned by AI

--- whisper.js
+++ whisper.js
@@ -16,6 +16,9 @@ const speakers = [
  * htmltopdf: https://www.npmjs.com/package/html2pdf.js
  */
 
+
+const sleep = seconds => new Promise(r => setTimeout(r, seconds * 1000));
+
 async function doStuff() {
     
     const fileName = `./media/${uuidv4()}.mp4`;
@@ -26,25 +29,60 @@ async function doStuff() {
     let loc = mp3File.indexOf('.mp3');
     if (loc === -1) return false;
     const jsonFile = mp3File.substring(0, loc) + '.json';
-    console.log('creating  raw transcript', mp3File);
+    // console.log("transcribing the mp3 into raw words");
     //const info = await deepgram.transcribeRecording(mp3File, jsonFile);
 
     if (info) {
-        const rawTranscript = deepgram.generateSpeakerBasedTranscript(info);
-        const speakerChunks = deepgram.getSpeakerChunks(rawTranscript);
-        const transcriptChunks = deepgram.getTranscriptChunks(speakerChunks);
+        console.log('generating raw transcript', mp3File);
+        let rawTranscript = deepgram.generateSpeakerBasedTranscript(info);
 
-        //console.log('assigning speakers', rawTranscript);
-        const speakerAssignedChunks = [];
+        console.log('splitting raw transcript into speaker-based paragraphs');
+        let speakerChunks = deepgram.getSpeakerChunks(rawTranscript);
+        rawTranscript = '';
+
+
+        console.log('creating AI-ready transcript chunks')
+        let transcriptChunks = deepgram.getTranscriptChunks(speakerChunks);
+        speakerChunks = null;
+
+        console.log('assigning speakers to AI chunks');
+        let speakerAssignedChunks = [];
         for (let i = 0; i < transcriptChunks.length; ++i) {
             console.log('chunk size', transcriptChunks[i].length, deepgram.getNumWords(transcriptChunks[i]));
             speakerAssignedChunks.push(deepgram.assignSpeakers(transcriptChunks[i], speakers));
         }
+        transcriptChunks = null;
 
-        console.log('speakerAssignedChunks', speakerAssignedChunks);
+       
 
-        //const rawChunks = deepgram.splitTranscriptIntoChunks(rawTranscript);
+        const cleanedChunks = [];
+        for (let i = 0; i < speakerAssignedChunks.length; ++i) {
+            console.log(`Using AI to clean up imperfections in transcript chunk #${i+1}. This can take several minutes.`);
+            let result;
+            /*
+             * IMPORTANT: check if response is serverTooBusy and retry after n minutes where n doubles each time.
+             * Send message, ChatGPT Server is Overloaded. Will retry in __ seconds (with countdown)
+             */
 
+            result = await ai.cleanTranscriptChunk(speakerAssignedChunks[i]);
+            let count = 0;
+            let seconds = 30;
+            const maxCount = 5;
+            while (result.status === 'error') {
+                seconds *= 2;
+                ++count;
+                if (count >= maxCount) return console.error(JSON.stringify(result.message,null, 4));
+                console.log(`ChatGPT Error. Waiting ${seconds} seconds and then trying again.`);
+                await sleep(seconds);
+                result = await ai.cleanTranscriptChunk(speakerAssignedChunks[i]);
+            }
+            
+            cleanedChunks.push(result.content);
+        }
+
+        console.log(cleanedChunks);
+
+        
 
         
         
